be66d1f742a5e74a18c345ffffe6cbc2c0602333
renjn <renjianan@aixuedai.com>
2018-01-31 09:43:11 +0800

    点击事件功能

--- app/src/main/java/com/renny/simplebrowser/view/page/RecyclerActivity.java
+++ app/src/main/java/com/renny/simplebrowser/view/page/RecyclerActivity.java
@@ -47,6 +47,7 @@ public class RecyclerActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         ToastHelper.makeToast("  点击了"+position);
+
                     }
                 });
             }
